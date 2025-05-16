b48ae44ba4b8a25ef78f3413edd3802574a07e71
sambit-mass <sambitkumar.choudhuri@massoftind.com>
2025-05-16 10:12:02 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
     const employee = { name, designation };
 
     try {
-      const response = await fetch('http://52.56.57.39:9090/api/employees', {
+      const response = await fetch('http://localhost:9090/api/employees', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
